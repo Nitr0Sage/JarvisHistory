62425838d3523eb7c8126a49d1ff2f0d34890278
Nitr0Sage <37257605+Nitr0Sage@users.noreply.github.com>
2022-01-31 21:31:29 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -30,8 +30,7 @@ class PersonalSkill(MycroftSkill):
 
     @intent_handler("WhoAreYou.intent")
     def handle_who_are_you_intent(self, message):
-        name = self.config_core.get("listener", {}).get("wake_word",
-                                                        "mycroft")
+        name = self.config_core.get("listener", {}).get("wake_word")
         name = name.lower().replace("hey ", "")
         self.speak_dialog("who.am.i", {"name": name})
 
